0c348a83f2361050ac225c3e78382c67ff9bee6f
oryo23-meet <109899438+oryo23-meet@users.noreply.github.com>
2022-08-01 13:20:38 +0300

    update

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,11 +10,12 @@ config = {
   "messagingSenderId": "928680021126",
   "appId": "1:928680021126:web:74cc478ef89e7683519507",
   "measurementId": "G-SK8WTZ2LWS",
-  "databaseURL": "",
+  "databaseURL": "https://fir-project-e1ccb-default-rtdb.europe-west1.firebasedatabase.app",
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database() 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -44,7 +45,12 @@ def signup():
 		password = request.form['password']
 		try:
 			login_session['user'] = auth.create_user_with_email_and_password(email, password)
-			return redirect(url_for('add_tweet'))
+			user = {
+			"email": request.form['email'], "password" : request.form['password'], "full_name" : request.form['full_name'],
+			"user_name" : request.form['user_name'], "bio" : request.form['bio']
+			}
+			db.child("users").child(login_session['user']["localId"]).set(user)
+			return redirect(url_for('signin'))
 		except:
 			error = "Authentication failed"
 			return redirect(url_for("signup"))
@@ -53,7 +59,18 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+	if request.method == 'POST':
+		tweet = {
+		"title" : request.form['title'], "bio" : request.form['bio'] 
+		}
+		db.child("articles").push(tweet)
+	return render_template("add_tweet.html")
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweet():
+	tweets = dict(db.child("tweets").get().val())
+
+	return render_template("tweets.html")
 
 
 if __name__ == '__main__':
